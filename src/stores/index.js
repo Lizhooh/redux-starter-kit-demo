9b93bb06614b9265f905f3b208235de0f9fc7351
lizhooh <960316408@qq.com>
2019-02-18 13:11:43 +0800

    updata code

--- src/stores/index.js
+++ src/stores/index.js
@@ -2,12 +2,22 @@ import { configureStore } from 'redux-starter-kit';
 import thunk from 'redux-thunk';
 import * as reducers from './reducers';
 
+const logger = () => next => action => {
+    const start = Date.now();
+    const result = next(action);
+    const end = Date.now();
+
+    console.log(`%c${action.type}  %c+${(end - start)}ms`,
+    'color:black;font-weight: 600;', 'color:green;font-weight: 600;');
+
+    return result;
+};
+
 const store = configureStore({
     devTools: true,
     reducer: reducers,
-    middleware: [thunk],
+    middleware: [thunk, logger],
 });
 
 export default store;
 
-console.log(store);
